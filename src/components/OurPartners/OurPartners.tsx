e4f9c2f322fd0186fa3022e2a6dd169a0032a80e
Umarali <umaraliomorov13@gmail.com>
2025-05-08 10:51:12 +0600

    feat: added sass and assest

---
+++ src/components/OurPartners/OurPartners.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './OurPartners.module.scss';
+
+export default function OurPartners() {
+    return (
+        <section className={styles.ourPartners}>
+            <div className={styles.top}></div>
+            <div className={styles.bottom}></div>
+        </section>
+    )
+}
