08a768f67ae42df641efff37d8b6cb282b4b0b34
Umarali <umaraliomorov13@gmail.com>
2025-05-15 15:09:00 +0600

    fixed telegram icon

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -8,14 +8,12 @@ import { SuccessShared } from "@/components/SuccessShared";
 import { Services } from "@/components/Services/Services";
 
 import { WhyPrimeSetup } from "@/components/WhyPrimeSetup";
-import { Telegram } from "@/ui/Telegram";
 import { UsefulArticles } from "@/components/UsefulArticles";
 
 
 export default function Home() {
   return (
     <>
-    <Telegram />
       <Hero
         imgUrl="/assets/images/heroBg.jpg"
         title="Ваш бизнес в надёжных руках"
