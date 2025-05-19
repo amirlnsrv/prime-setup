66f8b5346329103507c1ca130d618f1aeaeee868
Umarali <umaraliomorov13@gmail.com>
2025-05-19 13:12:09 +0600

    fix: helpers complated

--- src/mockData/mockData.ts
+++ src/mockData/mockData.ts
@@ -1,43 +1,36 @@
-
 // -----------------------------------------------------------
 
-
-
-const img1 = '/assets/icons/img1.svg'
-const img2 = '/assets/icons/img2.svg'
-const img3 = '/assets/icons/img3.svg'
-const img4 = '/assets/icons/img4.svg'
-const img5 = '/assets/icons/img5.svg'
-// const { title } = "process";
+import img1 from "#/icons/img1.svg";
+import img2 from "#/icons/img2.svg";
+import img3 from "#/icons/img3.svg";
+import img4 from "#/icons/img4.svg";
+import img5 from "#/icons/img5.svg";
+// import { title } from "process";
 
 // -----------------------------------------------------------------
-const notes = '/assets/icons/notes.svg'
-const companents2 = '/assets/icons/Component 1 (6).svg'
-const companents3 = '/assets/icons/Component 1 (7).svg'
-const companets4 = '/assets/icons/Component 1 (8).svg'
-const companets5 = '/assets/icons/Frame 34092 (8).svg'
-const companets6 = '/assets/icons/Component 1 (3).png'
-const companets7 = '/assets/icons/Component 1 (9).svg'
-const companents8 = '/assets/icons/Component 1 (10).svg'
+import notes from "#/icons/notes.svg";
+import companents2 from "#/icons/Component 1 (6).svg";
+import companents3 from "#/icons/Component 1 (7).svg";
+import companets4 from "#/icons/Component 1 (8).svg";
+import companets5 from "#/icons/Frame 34092 (8).svg";
+import companets6 from "#/icons/Component 1 (3).png";
+import companets7 from "#/icons/Component 1 (9).svg";
+import companents8 from "#/icons/Component 1 (10).svg";
 // ------------------------------------------------------------------------
-const medal = '/assets/icon/medalll.svg'
-const puzzule = '/assets/icons/puzzule.svg'
-const handshake = '/assets/icons/handshake.svg'
-const tame = '/assets/icon/tame.svg'
-const lisen = '/assets/icon/lisen.svg'
-const confidence = '/assets/icon/confidence.svg'
+import medal from "#/icon/medalll.svg";
+import puzzule from "#/icons/puzzule.svg";
+import handshake from "#/icons/handshake.svg";
+import tame from "#/icon/tame.svg";
+import lisen from "#/icon/lisen.svg";
+import confidence from "#/icon/confidence.svg";
 // ------------------------------------
-const medal2 = '/assets/icon/medal2.svg'
-const puzzule2 = '/assets/icon/puzzule2.svg'
-const handshake2 = '/assets/icon/hund2.svg'
-const tame2 = '/assets/icon/tame2.svg'
-const lisen2 = '/assets/icon/lisen2.svg'
-const confidence2 = '/assets/icon/confid.svg'
-// const { icon } = "./tempfile";
-
-
-
-
+import medal2 from "#/icon/medal2.svg";
+import puzzule2 from "#/icon/puzzule2.svg";
+import handshake2 from "#/icon/hund2.svg";
+import tame2 from "#/icon/tame2.svg";
+import lisen2 from "#/icon/lisen2.svg";
+import confidence2 from "#/icon/confid.svg";
+// import { icon } from "./tempfile";
 
 export const mockdataIcon = [
   {
@@ -45,21 +38,24 @@ export const mockdataIcon = [
     icon: medal,
     icon2: medal2,
     title: "15 лет опыта",
-    description: "С 2009 года успешно помогаем компаниям выходить на рынок ОАЭ.",
+    description:
+      "С 2009 года успешно помогаем компаниям выходить на рынок ОАЭ.",
   },
   {
     id: 2,
     icon: puzzule,
     icon2: puzzule2,
     title: "Комплексные решения",
-    description: "Все услуги для бизнеса в одном месте — от лицензии до банковского счёта.",
+    description:
+      "Все услуги для бизнеса в одном месте — от лицензии до банковского счёта.",
   },
   {
     id: 3,
     icon: handshake,
     icon2: handshake2,
     title: "Персональный подход",
-    description: "Решения, которые учитывают особенности вашего бизнеса и целей.",
+    description:
+      "Решения, которые учитывают особенности вашего бизнеса и целей.",
   },
   {
     id: 4,
@@ -80,88 +76,104 @@ export const mockdataIcon = [
     icon: confidence,
     icon2: confidence2,
     title: "Надёжные партнёры",
-    description: "Банки, госорганы и сервисы — для вашего удобства и безопасности.",
+    description:
+      "Банки, госорганы и сервисы — для вашего удобства и безопасности.",
   },
-]
+];
 
-export const BusinessSlider =[
+export const BusinessSlider = [
   {
-    id:1,
+    id: 1,
     imag: img1,
     title: "Бесплатная консультация",
-    description: "Мы начинаем с консультации, чтобы понять ваши цели и задачи. Обсуждаем, какие формы бизнеса подойдут именно вам, и подбираем оптимальный вариант для регистрации. Вы получите всю необходимую информацию для принятия решения.",
-  } ,{
-    id:2,
+    description:
+      "Мы начинаем с консультации, чтобы понять ваши цели и задачи. Обсуждаем, какие формы бизнеса подойдут именно вам, и подбираем оптимальный вариант для регистрации. Вы получите всю необходимую информацию для принятия решения.",
+  },
+  {
+    id: 2,
     imag: img2,
     title: "Персональное решение",
-    description: "На основе консультации мы разрабатываем индивидуальный план, учитывая ваши требования. Подбираем лицензию, оформляем документы и определяем сроки. Все действия согласуются с вами на каждом этапе.",
-  } ,{
-    id:3,
+    description:
+      "На основе консультации мы разрабатываем индивидуальный план, учитывая ваши требования. Подбираем лицензию, оформляем документы и определяем сроки. Все действия согласуются с вами на каждом этапе.",
+  },
+  {
+    id: 3,
     imag: img3,
     title: "Сбор и подготовка документов",
-    description: "Собираем и проверяем все необходимые документы, чтобы ваш бизнес был зарегистрирован без задержек. Мы оформляем все формы, заявления и удостоверяем их, если это требуется.",
-  },{
-    id:4,
+    description:
+      "Собираем и проверяем все необходимые документы, чтобы ваш бизнес был зарегистрирован без задержек. Мы оформляем все формы, заявления и удостоверяем их, если это требуется.",
+  },
+  {
+    id: 4,
     imag: img4,
     title: "Регистрация компании",
-    description: "После подготовки всех документов мы подаем их в соответствующие органы для регистрации. Следим за процессом и информируем вас на каждом этапе. В итоге, вы получаете свою лицензию и все официальные подтверждения.",
-  },{
-    id:5,
+    description:
+      "После подготовки всех документов мы подаем их в соответствующие органы для регистрации. Следим за процессом и информируем вас на каждом этапе. В итоге, вы получаете свою лицензию и все официальные подтверждения.",
+  },
+  {
+    id: 5,
     imag: img5,
     title: "Пост-регистрационное сопровождение",
-    description: "После регистрации мы не оставляем вас наедине с задачами. Помогаем открыть банковский счёт, оформить визы для сотрудников, предоставить бухгалтерское и юридическое сопровождение. Ваш бизнес будет развиваться без лишних забот.",
-  }
-]
+    description:
+      "После регистрации мы не оставляем вас наедине с задачами. Помогаем открыть банковский счёт, оформить визы для сотрудников, предоставить бухгалтерское и юридическое сопровождение. Ваш бизнес будет развиваться без лишних забот.",
+  },
+];
 
-
-
-
-
-
-export const mockdataSectiom =[
+export const mockdataSectiom = [
   {
-  id:1,
-  title:'Регистрация компаний в ОАЭ',
-  description:'Мы предлагаем бизнес под ключ: компании в свободных зонах, на материковой части и оффшорные компании.',
-img:notes,
-} , {
-  id:2,
-  title:'Открытие банковских счетов',
-  description:'Мы предлагаем бизнес под ключ: компании в свободных зонах, на материковой части и оффшорные компании.',
-img:companents2,
-}  ,{
-  id:3,
-  title:'Получение лицензий',
-  description:'Мы предлагаем бизнес под ключ: компании в свободных зонах, на материковой части и оффшорные компании.',
-img:companents3,
-} , {
-  id:4,
-  title:'Виртуальные офисы и коворкинги',
-  description:'Мы предлагаем бизнес под ключ: компании в свободных зонах, на материковой части и оффшорные компании.',
-img: companets4,
-},  {
-  id:5,
-  title:'Налоговое консультирование',
-  description:'Мы предлагаем бизнес под ключ: компании в свободных зонах, на материковой части и оффшорные компании.',
-img:companets5,
-},
+    id: 1,
+    title: "Регистрация компаний в ОАЭ",
+    description:
+      "Мы предлагаем бизнес под ключ: компании в свободных зонах, на материковой части и оффшорные компании.",
+    img: notes,
+  },
   {
-  id:6,
-  title:'Бухгалтерское сопровождение',
-  description:'Мы предлагаем бизнес под ключ: компании в свободных зонах, на материковой части и оффшорные компании.',
- img:companets6,
-}
-  ,{
-  id:7,
-  title:'Визы для инвесторов и сотрудников',
-  description:'Мы предлагаем бизнес под ключ: компании в свободных зонах, на материковой части и оффшорные компании.',
- img:companets7,
-}
-  ,{
-  id:8,
-  title:'Полное сопровождение бизнеса',
-  description:'Мы предлагаем бизнес под ключ: компании в свободных зонах, на материковой части и оффшорные компании.',
-img:companents8,
-  }
-
-]
+    id: 2,
+    title: "Открытие банковских счетов",
+    description:
+      "Мы предлагаем бизнес под ключ: компании в свободных зонах, на материковой части и оффшорные компании.",
+    img: companents2,
+  },
+  {
+    id: 3,
+    title: "Получение лицензий",
+    description:
+      "Мы предлагаем бизнес под ключ: компании в свободных зонах, на материковой части и оффшорные компании.",
+    img: companents3,
+  },
+  {
+    id: 4,
+    title: "Виртуальные офисы и коворкинги",
+    description:
+      "Мы предлагаем бизнес под ключ: компании в свободных зонах, на материковой части и оффшорные компании.",
+    img: companets4,
+  },
+  {
+    id: 5,
+    title: "Налоговое консультирование",
+    description:
+      "Мы предлагаем бизнес под ключ: компании в свободных зонах, на материковой части и оффшорные компании.",
+    img: companets5,
+  },
+  {
+    id: 6,
+    title: "Бухгалтерское сопровождение",
+    description:
+      "Мы предлагаем бизнес под ключ: компании в свободных зонах, на материковой части и оффшорные компании.",
+    img: companets6,
+  },
+  {
+    id: 7,
+    title: "Визы для инвесторов и сотрудников",
+    description:
+      "Мы предлагаем бизнес под ключ: компании в свободных зонах, на материковой части и оффшорные компании.",
+    img: companets7,
+  },
+  {
+    id: 8,
+    title: "Полное сопровождение бизнеса",
+    description:
+      "Мы предлагаем бизнес под ключ: компании в свободных зонах, на материковой части и оффшорные компании.",
+    img: companents8,
+  },
+];
