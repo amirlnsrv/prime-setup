da19a1da91c4f12e1cf041db20194d8b2ae9075b
Umarali <umaraliomorov13@gmail.com>
2025-05-19 13:41:11 +0600

    fix: fixed

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   webpack(config) {
     config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"],
     });
 
     return config;
