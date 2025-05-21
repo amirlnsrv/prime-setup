8abe733783952916bf301c03b6bcdc21a47c440e
Umarali <umaraliomorov13@gmail.com>
2025-05-21 11:42:44 +0600

    feat: modal complated

--- src/ui/PrimaryButton/PrimaryButton.tsx
+++ src/ui/PrimaryButton/PrimaryButton.tsx
@@ -1,23 +1,25 @@
 "use client";
 
 import styles from "./PrimaryButton.module.scss";
-
 import { useRouter } from "next/navigation";
 
 type Props = {
   className?: string;
   value: string;
   url?: string;
+  action?: () => void; // добавили action
 };
 
-export const PrimaryButton = ({ className, value, url }: Props) => {
+export const PrimaryButton = ({ className = "", value, url, action }: Props) => {
   const router = useRouter();
 
+  const handleClick = () => {
+    if (action) action();          // сначала вызываем action
+    if (url) router.push(url);     // потом переходим по ссылке
+  };
+
   return (
-    <button
-      onClick={() => router.push(`${url || "/"}`)}
-      className={`${styles.button} ${className}`}
-    >
+    <button onClick={handleClick} className={`${styles.button} ${className}`}>
       <span className={styles.txt}>{value}</span>
     </button>
   );
