57b42d0bc24fa4127bafd757454cc4722fe5821b
Umarali <umaraliomorov13@gmail.com>
2025-05-27 14:13:23 +0600

    fixed helpers

--- src/ui/ServiceCard/ServiceCard.tsx
+++ src/ui/ServiceCard/ServiceCard.tsx
@@ -1,6 +1,7 @@
 import styles from "./ServiceCard.module.scss";
 import { motion } from "framer-motion";
 import { useEffect, useState } from "react";
+import { desktopAnimations, mobileAnimations } from "./ServiceCard.helpers";
 
 export function ServiceCard({
   selectedIndex,
@@ -26,107 +27,7 @@ export function ServiceCard({
     return () => window.removeEventListener("resize", checkScreenSize);
   }, []);
 
-  const desktopAnimations = [
-    {
-      cube1: { x: 0, y: 0 },
-      cube2: { x: 0, y: 0 },
-      cube3: { x: 0, y: 0 },
-      cube4: { x: 0, y: 0 },
-    },
-    {
-      cube1: { x: 0, y: 50, scale: 0.8 },
-      cube2: { x: 0, y: 100, scale: 1.4 },
-      cube3: { x: -120, y: -50, scale: 0.8 },
-      cube4: { x: -80, y: -20, scale: 0.9 },
-    },
-    {
-      cube1: { x: 100, y: 30, scale: 0.8 },
-      cube2: { x: 100, y: -100, scale: 1.4 },
-      cube3: { x: -230, y: -20 },
-      cube4: { x: -220, y: -50 },
-    },
-    {
-      cube1: { x: 180, y: 100, scale: 0.8 },
-      cube2: { x: 160, y: -150, scale: 1.4 },
-      cube3: { x: -280, y: 50 },
-      cube4: { x: -250, y: -100, scale: 0.8 },
-    },
-    {
-      cube1: { x: 200, y: 200, scale: 0.5 },
-      cube2: { x: 350, y: -150, scale: 1.4 },
-      cube3: { x: -460, y: 70, scale: 1.8 },
-      cube4: { x: -300, y: -280, scale: 0.8 },
-    },
-    {
-      cube1: { x: 200, y: 330, scale: 0.5 },
-      cube2: { x: 410, y: -30, scale: 1.5 },
-      cube3: { x: -570, y: -50, scale: 1.8 },
-      cube4: { x: -250, y: -400, scale: 0.8 },
-    },
-    {
-      cube1: { x: 60, y: 470, scale: 0.8 },
-      cube2: { x: 520, y: 150, scale: 1 },
-      cube3: { x: -500, y: -230, scale: 1.5 },
-      cube4: { x: -100, y: -550, scale: 0.8 },
-    },
-    {
-      cube1: { x: -100, y: 630, scale: 0.7 },
-      cube2: { x: 300, y: 250, scale: 1.4 },
-      cube3: { x: -400, y: -400, scale: 1.4 },
-      cube4: { x: 50, y: -590, scale: 0.6 },
-    },
-  ];
 
-  const mobileAnimations = [
-    {
-      cube1: { x: 0, y: 0, scale: 0.8 },
-      cube2: { x: 0, y: 0, scale: 0.8 },
-      cube3: { x: 0, y: 0, scale: 0.8 },
-      cube4: { x: 0, y: 0, scale: 0.8 },
-    },
-    {
-      cube1: { x: 0, y: 30, scale: 0.5 },
-      cube2: { x: 0, y: 60, scale: 1 },
-      cube3: { x: -80, y: -30, scale: 0.5 },
-      cube4: { x: -50, y: -15, scale: 0.7 },
-    },
-    {
-      cube1: { x: 60, y: 20, scale: 0.5 },
-      cube2: { x: 60, y: -60, scale: 1 },
-      cube3: { x: -150, y: -15, scale: 0.5},
-      cube4: { x: -140, y: -30, scale: 0.7},
-    },
-    {
-      cube1: { x: 100, y: 60, scale: 0.5 },
-      cube2: { x: 90, y: -90, scale: 1 },
-      cube3: { x: -180, y: 30, scale: 0.8 },
-      cube4: { x: -160, y: -60, scale: 0.5 },
-    },
-    {
-      cube1: { x: 120, y: 120, scale: 0.5 },
-      cube2: { x: 200, y: -90, scale: 1 },
-      cube3: { x: -280, y: 40, scale: 1.2 },
-      cube4: { x: -180, y: -170, scale: 0.5 },
-    },
-    {
-      cube1: { x: 120, y: 150, scale: 0.3 },
-      cube2: { x: 240, y: -120, scale: 1 },
-      cube3: { x: -340, y: -30, scale: 1.2 },
-      cube4: { x: -150, y: -240, scale: 0.5 },
-    },
-    {
-      cube1: { x: 40, y: 380, scale: 0.5 },
-      cube2: { x: 350, y: -90, scale: 0.8 },
-      cube3: { x: -300, y: -0, scale: 1 },
-      cube4: { x: -60, y: -380, scale: 0.5 },
-    },
-    {
-      cube1: { x: -160, y: 380, scale: 0.5 },
-      cube2: { x: 250, y: 250, scale: 1 },
-      cube3: { x: -260, y: -340, scale: 1 },
-      cube4: { x: 60, y: -350, scale: 0.4 },
-    },
-  ];
 
   const cubeAnimations = isSmallScreen ? mobileAnimations : desktopAnimations;
 
