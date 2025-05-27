6f9ab01f11ae24a38248fb9af5801870ba78a804
Umarali <umaraliomorov13@gmail.com>
2025-05-27 13:25:58 +0600

    feat: added adaptation

--- src/ui/ServiceCard/ServiceCard.tsx
+++ src/ui/ServiceCard/ServiceCard.tsx
@@ -1,5 +1,6 @@
 import styles from "./ServiceCard.module.scss";
 import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
 
 export function ServiceCard({
   selectedIndex,
@@ -8,8 +9,24 @@ export function ServiceCard({
   selectedIndex: number;
   services: { title: string; content: string }[];
 }) {
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
 
-  const cubeAnimations = [
+  useEffect(() => {
+    const checkScreenSize = () => {
+      setIsSmallScreen(window.innerWidth < 1300);
+    };
+
+    // Проверяем при монтировании
+    checkScreenSize();
+
+    // Добавляем слушатель изменений размера
+    window.addEventListener("resize", checkScreenSize);
+
+    // Убираем слушатель при размонтировании
+    return () => window.removeEventListener("resize", checkScreenSize);
+  }, []);
+
+  const desktopAnimations = [
     {
       cube1: { x: 0, y: 0 },
       cube2: { x: 0, y: 0 },
@@ -59,6 +76,60 @@ export function ServiceCard({
       cube4: { x: 50, y: -590, scale: 0.6 },
     },
   ];
+
+  const mobileAnimations = [
+    {
+      cube1: { x: 0, y: 0, scale: 0.8 },
+      cube2: { x: 0, y: 0, scale: 0.8 },
+      cube3: { x: 0, y: 0, scale: 0.8 },
+      cube4: { x: 0, y: 0, scale: 0.8 },
+    },
+    {
+      cube1: { x: 0, y: 30, scale: 0.5 },
+      cube2: { x: 0, y: 60, scale: 1 },
+      cube3: { x: -80, y: -30, scale: 0.5 },
+      cube4: { x: -50, y: -15, scale: 0.7 },
+    },
+    {
+      cube1: { x: 60, y: 20, scale: 0.5 },
+      cube2: { x: 60, y: -60, scale: 1 },
+      cube3: { x: -150, y: -15, scale: 0.5},
+      cube4: { x: -140, y: -30, scale: 0.7},
+    },
+    {
+      cube1: { x: 100, y: 60, scale: 0.5 },
+      cube2: { x: 90, y: -90, scale: 1 },
+      cube3: { x: -180, y: 30, scale: 0.8 },
+      cube4: { x: -160, y: -60, scale: 0.5 },
+    },
+    {
+      cube1: { x: 120, y: 120, scale: 0.5 },
+      cube2: { x: 200, y: -90, scale: 1 },
+      cube3: { x: -280, y: 40, scale: 1.2 },
+      cube4: { x: -180, y: -170, scale: 0.5 },
+    },
+    {
+      cube1: { x: 120, y: 150, scale: 0.3 },
+      cube2: { x: 240, y: -120, scale: 1 },
+      cube3: { x: -340, y: -30, scale: 1.2 },
+      cube4: { x: -150, y: -240, scale: 0.5 },
+    },
+    {
+      cube1: { x: 40, y: 380, scale: 0.5 },
+      cube2: { x: 350, y: -90, scale: 0.8 },
+      cube3: { x: -300, y: -0, scale: 1 },
+      cube4: { x: -60, y: -380, scale: 0.5 },
+    },
+    {
+      cube1: { x: -160, y: 380, scale: 0.5 },
+      cube2: { x: 250, y: 250, scale: 1 },
+      cube3: { x: -260, y: -340, scale: 1 },
+      cube4: { x: 60, y: -350, scale: 0.4 },
+    },
+  ];
+
+  const cubeAnimations = isSmallScreen ? mobileAnimations : desktopAnimations;
+
   return (
     <div className={styles.contentPanel}>
       <motion.div
