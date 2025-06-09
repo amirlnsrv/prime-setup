2a4138ec2248a19528b917c122601f63480d222a
Umarali <umaraliomorov13@gmail.com>
2025-06-09 23:37:47 +0600

    feat: added drug and drop

--- src/app/admin/add-article/page.tsx
+++ src/app/admin/add-article/page.tsx
@@ -10,17 +10,46 @@ import Button from "@/ui/Button";
 export default function AddArticle() {
   const [previewImage, setPreviewImage] = useState<string | null>(null);
   const [isOpen, setIsOpen] = useState(false);
-  const closeModal = () => {
-    setIsOpen(false);
-  };
-  const openModal = () => {
-    setIsOpen(true);
-  };
+  const [isDragging, setIsDragging] = useState(false);
+  const [isCurrentSize, setIsCurrentSize] = useState(true);
+
+  const closeModal = () => setIsOpen(false);
+  const openModal = () => setIsOpen(true);
+
   const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
+    processFile(file);
+  };
+
+  const handleDeleteImage = () => {
+    setPreviewImage(null);
+    setIsCurrentSize(true); // сброс ошибки размера (если была)
+  };
+
+  const handleDrop = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    setIsDragging(false);
+    const file = e.dataTransfer.files?.[0];
+    processFile(file);
+  };
+
+  const processFile = (file?: File) => {
     if (file && file.type.startsWith("image/")) {
-      const imageUrl = URL.createObjectURL(file);
-      setPreviewImage(imageUrl);
+      if (file.size <= 2 * 1024 * 1024) {
+        const imageUrl = URL.createObjectURL(file);
+        setPreviewImage(imageUrl);
+        setIsCurrentSize(false);
+      } else {
+        console.log("error");
+
+        setIsCurrentSize(false); // устанавливаем false при ошибке размера
+        setPreviewImage(null); // сбрасываем превью
+        setTimeout(() => {
+          setIsCurrentSize(true); // сбрасываем ошибку размера через 2 секунды
+        }, 2000);
+      }
+    } else {
+      alert("Пожалуйста, выберите изображение.");
     }
   };
 
@@ -38,21 +67,57 @@ export default function AddArticle() {
         />
 
         <p>Обложка статьи</p>
-        <div className={styles.imageWrap}>
+        <div
+          className={`${styles.imageWrap} ${
+            isDragging ? styles.dragging : ""
+          } ${previewImage ? styles.filled : ""}`}
+          onDragOver={(e) => {
+            e.preventDefault();
+            setIsDragging(true);
+          }}
+          onDragLeave={() => setIsDragging(false)}
+          onDrop={handleDrop}
+        >
           {previewImage ? (
-            <Image src={previewImage} alt="Обложка статьи" fill />
+            <div className={styles.previewImage}>
+              <Image
+                className={styles.img}
+                src={previewImage}
+                alt="Обложка статьи"
+                fill
+              />
+              <button
+                className={styles.deleteImage}
+                onClick={handleDeleteImage}
+                type="button"
+              >
+                Удалить изображение
+              </button>
+            </div>
           ) : (
-            <Image
-              src={emptyIcon}
-              alt="image"
-              width={100}
-              height={100}
-            />
+            <>
+              <Image
+                src={emptyIcon}
+                alt="Пустая обложка"
+                width={100}
+                height={100}
+              />
+              <label className={styles.imageLabel} htmlFor="picture">
+                <span className={isCurrentSize ? "" : styles.errorLoad}>
+                  Загрузите изображение для статьи
+                </span>
+                <span className={isCurrentSize ? "" : styles.error}>
+                  Обложка должна быть не больше 2 MB
+                </span>
+              </label>
+            </>
           )}
-          <label className={styles.imageLabel} htmlFor="picture">
+
+          {/* <label className={styles.imageLabel} htmlFor="picture">
             <span>Загрузите изображение для статьи</span>
             <span>Обложка должна быть не больше 2 MB</span>
-          </label>
+          </label> */}
+
           <input
             type="file"
             id="picture"
@@ -76,10 +141,12 @@ export default function AddArticle() {
           className={styles.input}
           type="datetime-local"
         />
+
         <div className={styles.buttons}>
           <Button handleClick={openModal} value="Отмена" />
           <Button link="/admin" value="Опубликовать" />
         </div>
+
         {isOpen && (
           <Modal
             closeModal={closeModal}
